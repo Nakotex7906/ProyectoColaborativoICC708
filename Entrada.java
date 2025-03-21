ff2e148ab9425beeb00a139188b1d09d89e5dd95
Ignacio essus <i.essus01@ufromail.cl>
2025-03-21 13:17:46 -0300

    Clase que recibe lista de notas

---
+++ Entrada.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Entrada {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Double> calificaciones = new ArrayList<>();
+
+        System.out.println("Ingrese las calificaciones (0 para terminar)");
+
+        while (true) { 
+            double calificacion = scanner.nextDouble();
+            if(calificacion == 0) break;
+            calificaciones.add(calificacion);
+        }
+        scanner.close();
+
+        System.out.println("Las notas son: " + calificaciones);
+
+    }
+
+}
