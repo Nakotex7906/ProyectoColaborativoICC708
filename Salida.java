1efc5b822543a8f1133df34721e1080a8e1a3d5f
BenjaBeroiza <b.beroiza01@ufromail.cl>
2025-03-23 15:23:28 -0300

    salida de datos v.2

--- Salida.java
+++ Salida.java
@@ -1,1 +1,9 @@
-public class Salida 
+public class Salida {
+
+    public static void imprimirResultado(String resultado){
+
+        System.out.printnl("El promedio de las notas es: "+ calificaciones)
+    }
+
+//ola
+}
