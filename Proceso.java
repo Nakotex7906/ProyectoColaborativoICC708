bb98ecdd463ca90a54a96b2d0e2c7f3ecaaded77
Alonso <w.chavez01@ufromail.cl>
2025-03-23 15:46:19 -0300

    Cambio a Metodo

--- Proceso.java
+++ Proceso.java
@@ -1,39 +1,15 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
-public class Proceso{
-    public static void main(String[] args){
+public class Proceso {
 
-        Scanner scanner = new Scanner(System.in);
-        Arraylist<Double> calificaciones = new Arraylist<>();
-
-        System.out.println("Ingrese las calificaciones (0 para terminar)");
+    public static double calcularPromedio(ArrayList<Double> calificaciones) {
         
-        while(true){
-            double calificaciones = scanner.nextDouble();
-            
-            if (calificacion == 0) break;
-            calificaciones.add(calificacion);
+        if (calificaciones.isEmpty()) return 0;
+        
+        double suma = 0;
+        for (double nota : calificaciones) {
+            suma += nota;
         }
-
-        scanner.close();
-
-        System.out.println("Las notas son: " + calificaciones);
-
-        if(!calificaciones.is Empty()){
-           
-            double suma = 0;
-            double promedio = 0;
-
-            for(double c : calificaciones) {
-                suma += c;
-            }
-            promedio = (suma / calificaciones.size())
-
-    
-        } 
-
-
-    }
-
+        return suma / calificaciones.size();
+    }
 }
